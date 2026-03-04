1f84861a29c1f9e65ab85c6396fe6ab508c4d0cb
Muhammad Nauman <maliknomigep@gmail.com>
2026-03-04 18:50:17 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter number";
+    cin>>n;
+    for(int i=1;i<=10;i++){
+
+            cout<<n*i<<" ";
+    }
+    return 0;
+}
